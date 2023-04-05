213b3962017906a7e472dfb667f018f0e452e09f
Coder Juhi <juhichin505@gmail.com>
2023-04-04 22:25:26 -0400

    working on winner and exporting data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,6 +8,8 @@ candidate_list = []
 candidate_votes = {}
  #{name: votes}
  #candidate_votes[name of candidate]=votes --> {name:votes}
+ #with/as is used for opening files
+ 
 with open (election_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
@@ -15,16 +17,22 @@ with open (election_data, 'r') as csvfile:
     for index, row in enumerate(csvreader):
         #index = 0, row = 1323913,Jefferson,Charles Casper Stockham
         votes = votes + 1
+        
         if row[2] not in candidate_list:
             candidate_list.append(row[2])
+            
         if row[2] not in candidate_votes:
             candidate_votes[row[2]] = 1
         else:
             candidate_votes[row[2]] = candidate_votes[row[2]] + 1
-       
-print(f"Election Results")
-print(f"-------------------")
-print(f"Total Votes: {votes}")
-print(f"-------------------")
-print(f"{len(candidate_list)}")
+        
+    print(f"Election Results")
+    print(f"-------------------")
+    print(f"Total Votes: {votes}")
+    print(f"-------------------")
+    for key in candidate_votes:
+        percentage = (candidate_votes[key]/votes)*100
+        print(key, '',("{:0.3f}" .format(percentage)),"%" ,'', candidate_votes[key])
+    print(f"-------------------")
+    print(f"Winner: ")
     
